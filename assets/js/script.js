9f91cbd9bc37ba69afbf63224254a95d58351683
Chuca <Chuca78@users.noreply.github.com>
2022-10-22 14:09:27 -0400

    cleaned up CSS, current and forecast (today) done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,13 @@
 
 const currentWeatherEl = document.getElementById('currentWeather');
-console.log(currentWeatherEl)
 const cityEl = document.getElementById('city');
-console.log(cityEl)
 const coordsEl = document.getElementById('coords');
-console.log(coordsEl)
 const currentTempEl = document.getElementById('currentTemp');
-console.log(currentTempEl)
 const currentWindEl = document.getElementById('currentWind');
-console.log(currentWindEl)
 const currentHumidityEl = document.getElementById('currentHumidity');
-console.log(currentHumidityEl)
-
-
+const todayForecastEl = document.getElementById('today');
+const forecastEl = document.getElementById('forecast');
+console.log(forecastEl);
 
 // displays current day and date in the header
 var today = moment();
@@ -20,8 +15,7 @@ $("#currentTime").text(today.format("hh:mm A, dddd, MMM Do, YYYY"));
 
 
 
-// Getting weather information based on user location
-
+// this function uses geolocation to get current weather data
 getWeather()
 
 function getWeather(){
@@ -37,51 +31,103 @@ function getWeather(){
     console.log(latitude);
     console.log(longitude);
 
-// todo: use these variables to get current weather
-var city_current=data.name
-var temp_current= Math.floor((data.main.temp));
-var humidity_current=data.main.humidity
-var wind_current=data.wind.speed
+// todo: use these variables to retrieve current weather
+    var city_current=data.name
+    var temp_current= Math.floor((data.main.temp));
+    var humidity_current=data.main.humidity
+    var wind_current=data.wind.speed
 
-console.log(city_current);
-console.log(temp_current);
-console.log(humidity_current);
-console.log(wind_current);
+    console.log(city_current);
+    console.log(temp_current);
+    console.log(humidity_current);
+    console.log(wind_current);
 
+    // todo: fix city display
+    cityEl.innerHTML = 
+    `<div class="cityLabel" id=city></div><div>${city_current}</div>`;
 
-    // showWeatherData(data);
-})
+    // this displays current weather conditions
+    currentWeatherEl.innerHTML = 
+    `<h3>Current Weather</h3>
+    <div class="weatherItem" id="currentTemperature">
+    <div>Temperature</div>
+    <div>${temp_current}\u00B0F</div>
+    </div>
+    <div class="weatherItem" id="currentWindSpeed">
+    <div>Wind Speed</div><div>${wind_current} mph</div>
+    </div>
+    <div class="weatherItem" id="currentHumidity">
+    <div>Humidity</div>
+    <div>${humidity_current}%</div>
+    </div>`;
+
+    })
 }
 }
 
-// this function uses geolocation to get weather data
+// todo: fix this
+// today's weather forecast
+
+    // data.daily.forEach((today, idx) => {
+    //     if(idx == 0){
+todayForecastEl.innerHTML =
+// <img src="http://openweathermap.org/img/wn//${today.weather[0].icon}@4x.png" alt="weather icon" class="w-icon">
+ `<div class="todayForecast">
+    <div class="dayLabel">${window.moment().format("dddd")}</div>
+    <div class="weatherItem" id="temp">
+        <div>Temp</div>
+        <div>97 \u00B0F</div></div>
+    <div class="weatherItem" id="windSpeed">
+    <div>Wind</div>
+    <div>50 mph</div></div>
+    <div class="weatherItem" id="humidity">
+    <div>Humidity</div>
+    <div>0%</div></div>`
+
+
+
+// todo: fix the forecast code
+// forecast for the next 5 days
+
+forecastEl.innerHTML = 
+`<div class="futureDay1">
+<img src="http://openweathermap.org/img/wn/10d@2x.png" alt="weather-icon" class="weatherIcon">
+    <div class="dayLabel">${window.moment().add(1,'days').format("dddd")}</div>
+    <div class="weatherItem" id="temp"><p>Temp</p><p>97 \u00B0F</p></div>
+    <div class="weatherItem" id="windSpeed"><p>Wind</p><p>50 mph</p></div>
+    <div class="weatherItem" id="humidity"><p>Humidity</p><p>0%</p></div>`
+
+    // `<div class="futureDay2">
+
+    // <div class="dayLabel">${window.moment().add(2,'days').format("dddd")}</div>
+    // <div class="weatherItem" id="temp"><p>temperature</p><p>97</p></div>
+    // <div class="weatherItem" id="windSpeed"><p>wind speed</p><p>50</p></div>
+    // <div class="weatherItem" id="humidity"><p>humidity</p><p>0%</p></div>`
 
 
 
 
-// todo: this code doesn't work yet
-// function showWeatherData (data){
-//     let {humidity, temp, wind_speed} = data.current;
-//     console.log(humidity)
-//     console.log(temp)
-//     console.log(wind_speed)
+// {<div class="weatherItem" id="currentTemperature">Temperature</div><div>${today.temp}\u00B0 F</div> }
 
-//     currentWeatherEl.innerHTML = 
-//     `<div class="weather-item">
-//         <div>Humidity</div>
-//         <div>${humidity}%</div>
-//     </div>
-//     <div class="weather-item">
-//         <div>Temperature</div>
-//         <div>${temp}</div>
-//     </div>
-//     <div class="weather-item">
-//         <div>Wind Speed</div>
-//         <div>${wind_speed}</div>
-//     </div>`;
+// Getting weather forecast based on user location
+// forecastWeather()
+
+// function forecastWeather(){
+//     navigator.geolocation.getCurrentPosition(success);
+//     function success(position) {
+  
+//     latitude=position.coords.latitude;
+//     longitude=position.coords.longitude;
+
+//     fetch('http://api.openweathermap.org/data/2.5/forecast?lat=' + latitude + "&lon=" + longitude + '&units=imperial&APPID=e55c7e382f794c24ff4d937e890f4431').then(res => res.json()).then(data => {
+
+//     console.log(data);
+//     console.log(latitude);
+//     console.log(longitude);
+
+
+
 // }
-
-
 
 
 
@@ -90,11 +136,8 @@ console.log(wind_current);
 // function to show weather data in other locations
 
 
-
-
-
-
-
+// api call by city
+// fetch('https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}');
 
 
 // todo: create a local storage history function here
